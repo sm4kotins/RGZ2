d2f96646cca2f7012394aec97d41fa79ac3e3afa
Егор Панченко <carecasebas@hotmail.com>
2023-09-13 16:43:29 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return  "web-сервер на flask"
+    return  """
+<!doctype html>
+<html>
+    <head>
+        <title>Панченко Егор Михайлович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Егор Панченко, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
