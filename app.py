93dae2b01adbe21dfef045c471ff330a9688a1fc
Егор Панченко <carecasebas@hotmail.com>
2023-09-17 20:50:17 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -115,8 +115,12 @@ def toner():
 
 @app.route('/lab2/example')
 def example():
-    name='Егор Панченко'
-    number='2'
-    groupe='ФБИ-13'
-    course='3 курс'
-    return render_template('example.html', name=name, number=number, groupe=groupe, course=course)
+    name, number, groupe, course='Егор Панченко', 2, 'ФБИ-13', '3 курс'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95}, 
+        {'name': 'манго', 'price': 321},
+    ]
+    return render_template('example.html', name=name, number=number, groupe=groupe, course=course, fruits=fruits)
