039b2e46ca70d287a7cc50e2e147661ebb948e9f
Егор Панченко <carecasebas@hotmail.com>
2023-09-17 21:11:44 +0700

    Книги

--- app.py
+++ app.py
@@ -123,4 +123,16 @@ def example():
         {'name': 'мандарины', 'price': 95}, 
         {'name': 'манго', 'price': 321},
     ]
-    return render_template('example.html', name=name, number=number, groupe=groupe, course=course, fruits=fruits)
+    books = [
+        {'author': 'Джеймс Джойс', 'name': 'Улисс', 'genre': 'роман', 'pages': '1056'},
+        {'author': 'Драйзер Т.', 'name': 'Трилогия желания', 'genre': 'роман', 'pages': '1152'},
+        {'author': 'Толстой Л.', 'name': 'Война и мир', 'genre': 'роман-эпопея', 'pages': '1360'},
+        {'author': 'Голсуорси Дж.', 'name': 'Сага о Форсайтах', 'genre': 'роман', 'pages': '1376'},
+        {'author': 'Солженицын А.', 'name': 'Архипелаг ГУЛАГ', 'genre': 'роман', 'pages': '1424'},
+        {'author': 'Палиссер Ч.', 'name': 'Квинканкс', 'genre': 'роман', 'pages': '1472'},
+        {'author': 'Манн Т.', 'name': 'Иосиф и его братья', 'genre': 'роман', 'pages': '1492'},
+        {'author': 'Музиль Р.', 'name': 'Человек без свойств', 'genre': 'роман', 'pages': '1774'},
+        {'author': 'Пруст М.', 'name': 'В поисках утраченного времени', 'genre': 'роман', 'pages': '3031'},
+        {'author': 'Кнаусгор К.', 'name': 'Моя борьба', 'genre': 'биография', 'pages': '3600'},
+    ]
+    return render_template('example.html', name=name, number=number, groupe=groupe, course=course, fruits=fruits, books=books)
