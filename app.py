d1bec8685e6503b04190810c74bc2d8895d40002
Егор Панченко <carecasebas@hotmail.com>
2023-12-22 22:08:18 +0700

    Заготовка к 7 лабораторной

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 
 app = Flask(__name__)
@@ -40,3 +41,4 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
